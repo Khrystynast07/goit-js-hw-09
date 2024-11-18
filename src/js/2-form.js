d7971240ab7cae82c31727428b44f5d8948bd6d7
Khrystynast07 <khrystynastruk23@gmail.com>
2024-11-17 20:27:35 -0500

    hw form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,24 @@
-console.log("Form");
+const formData = "feedback-msg";
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input');
+const messageInput = form.querySelector('textarea');
+
+form.addEventListener('submit', handleSubmit);
+emailInput.addEventListener('input', onEmailInput);
+messageInput.addEventListener('textarea', onTextareaMsg);
+
+function onEmailInput(event) {
+    const value = event.target.value;
+    localStorage.setItem(formData, value);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if(!formData.email || !formData.message) {
+        alert('Please, fill all fields');
+        return;
+    }
+    event.currentTarget.reset();
+    localStorage.removeItem(formData);
+}
