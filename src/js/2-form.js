43f7ebbb2a997d55923c4af4f901e81c8b42b9d4
Khrystynast07 <khrystynastruk23@gmail.com>
2024-11-19 22:14:39 -0500

    update 2-form js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,35 +1,41 @@
-const formData = {email: "", message: ""};
-const form = document.querySelector(".feedback-form");
+const formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input');
 const messageInput = form.querySelector('textarea');
-
-emailInput.value = formData.email = JSON.parse(localStorage.getItem('feedback-form-state')).email || "";
-messageInput.value = formData.message = JSON.parse(localStorage.getItem('feedback-form-state')).message || "";;
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('input', handleInput);
 
-function savedToStorage() {
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
 }
+loadFormData();
+
 
 function handleInput(event) {
-    const {name, value} = event.target;
-    formData[name] = value;
-    savedToStorage();
+  const { name, value } = event.target;
+  formData[name] = value;
+  savedToStorage();
+}
+
+function savedToStorage() {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData ));
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if(!formData.email || !formData.message) {
-        alert('Please, fill all fields');
-        return;
-    }
-    event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    formData.email = "";
-    formData.message = "";
-
-    
+  if (!formData.email || !formData.message) {
+    alert('Please, fill all fields');
+    return;
+  }
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
 }
